b047302972f17922eb38174f0c950c93b9a8309b
KAL <kevin12147@yahoo.com.tw>
2023-12-07 06:05:38 +0800

    bug fixed

--- app/(site)/components/Member.tsx
+++ app/(site)/components/Member.tsx
@@ -1,5 +1,47 @@
 import prisma from "@/app/libs/prismadb";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import weekday from "dayjs/plugin/weekday";
+
 import ListOfMembers from "./ListOfMembers";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(weekday);
+
+let date = dayjs(new Date()).tz("Asia/Taipei");
+
+async function getTimeStamp() {
+  const res = await prisma.reset.findMany();
+  if (res.length) {
+    if (
+      dayjs().weekday(2).format("YYYY-MM-DD") !== res[0].timeStamp &&
+      dayjs().day() !== 0
+    ) {
+      await prisma.member.updateMany({
+        where: {},
+        data: {
+          sevendaysum: 0,
+          undoSevendaysum: 0,
+        },
+      });
+      await prisma.reset.updateMany({
+        where: {},
+        data: {
+          timeStamp: dayjs().weekday(2).format("YYYY-MM-DD"),
+        },
+      });
+    }
+  } else {
+    await prisma.reset.create({
+      data: {
+        timeStamp: dayjs().weekday(2).format("YYYY-MM-DD"),
+      },
+    });
+  }
+  return null;
+}
 
 async function getMember() {
   const res = await prisma.member.findMany({
@@ -13,5 +55,6 @@ async function getMember() {
 
 export default async function Members() {
   const member = await getMember();
+  const timeStamp = await getTimeStamp();
   return <ListOfMembers members={member} />;
 }
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,53 +1,10 @@
 import prisma from "@/app/libs/prismadb";
-import dayjs from "dayjs";
-import utc from "dayjs/plugin/utc";
-import timezone from "dayjs/plugin/timezone";
-import weekday from "dayjs/plugin/weekday";
 import { Suspense } from "react";
-
 import Members from "./components/Member";
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-dayjs.extend(weekday);
-
-let date = dayjs(new Date()).tz("Asia/Taipei");
 
 export const revalidate = 0;
 
-async function getTimeStamp() {
-  const res = await prisma.reset.findMany();
-  if (res.length) {
-    if (
-      dayjs().weekday(2).format("YYYY-MM-DD") !== res[0].timeStamp &&
-      dayjs().day() !== 0
-    ) {
-      await prisma.member.updateMany({
-        where: {},
-        data: {
-          sevendaysum: 0,
-          undoSevendaysum: 0,
-        },
-      });
-      await prisma.reset.updateMany({
-        where: {},
-        data: {
-          timeStamp: dayjs().weekday(2).format("YYYY-MM-DD"),
-        },
-      });
-    }
-  } else {
-    await prisma.reset.create({
-      data: {
-        timeStamp: dayjs().weekday(2).format("YYYY-MM-DD"),
-      },
-    });
-  }
-  return null;
-}
-
 export default async function Home() {
-  const timeStamp = await getTimeStamp();
   return (
     <>
       <Suspense fallback={<div>Loading data...</div>}>
